379aa10f9f25ea594fb51cba9e0e8aa067d9b430
notyeninja <notyeninja@gmail.com>
2020-02-02 13:53:40 -0800

    git card initial styling and composition

--- src/GitCard.js
+++ src/GitCard.js
@@ -1,36 +1,63 @@
 import React from 'react';
 
-function GitCard(){
-    return(
-        <article className="media has-background-light">
+function GitCard() {
+  return (
+    <div className="box has-background-light">
+      <article className="media">
         <figure className="media-left">
           <p className="image is-96x96">
-            <img src="https://bulma.io/images/placeholders/128x128.png" />
+            <img src="https://avatars3.githubusercontent.com/u/139426?v=4" />
           </p>
         </figure>
         <div className="media-content">
           <div className="content">
-            <p>
-              <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
-              <br />
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
-            </p>
+            <h1 className="title is-3">angular</h1>
+            <h6 className="subtitle is-primary">One framework. Mobile & desktop.</h6>
+
           </div>
           <nav className="level is-mobile">
             <div className="level-left">
-              <a className="level-item">
-                <span className="icon is-small"><i className="fas fa-reply"></i></span>
-              </a>
-              <a className="level-item">
-                <span className="icon is-small"><i className="fas fa-retweet"></i></span>
-              </a>
-              <a className="level-item">
-                <span className="icon is-small"><i className="fas fa-heart"></i></span>
-              </a>
+              <div className="level-item has-text-centered">
+                <div>
+                  <p className="heading">Size</p>
+                  <p className="subtitle">13K</p>
+                </div>
+              </div>
+              <div className="level-item has-text-centered">
+                <div>
+                  <p className="heading">Forks</p>
+                  <p className="subtitle">16K</p>
+                </div>
+              </div>
+              <div className="level-item has-text-centered">
+                <div>
+                  <p className="heading">Score</p>
+                  <p className="subtitle">18K</p>
+                </div>
+              </div>
+            </div>
+            <div className="level-right">
+              <div className="level-item">
+                <button className="button is-primary is-outlined">
+                  <span className="icon"><i className="fab fa-github"></i></span>
+                  <span>Check Them Out!</span>
+                 </button>
+              </div>
+              <div className="level-item">
+                <button className="button is-primary is-outlined">
+                  <span className="icon"><i className="far fa-thumbs-up"></i></span>
+                 </button>
+              </div>
+              <div className="level-item">
+                <button className="button is-danger is-outlined">
+                  <span className="icon"><i className="far fa-thumbs-down"></i></span>
+                 </button>
+              </div>
             </div>
           </nav>
         </div>
       </article>
-    );
+    </div>
+  );
 }
 export default GitCard;
---
+++ src/RepoHome.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function RepoHome(){
+    return(
+        null
+    );
+}
+
+export default RepoHome;
