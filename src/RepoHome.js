d9cacd2a8feb3552462969adcfb1205f85b44e58
notyeninja <notyeninja@gmail.com>
2020-02-03 03:15:29 -0800

    style changes for tablet form

--- src/RepoHome.js
+++ src/RepoHome.js
@@ -9,52 +9,60 @@ import Search from './Search';
 function RepoHome() {
     const [repoList, setRepoList] = useState([]);
 
-    async function searchRepository(searchTerm){
+    async function searchRepository(searchTerm) {
         const result = await repoService.searchRepo(searchTerm);
         setRepoList(result);
     }
 
     /** TODO: Convert this into a Redux pattern when time permits. */
-    function updateLikes(repoId,change){
+    function updateLikes(repoId, change) {
         debugger;
         let repo = findRepo(repoId);
-        if( repo !== null){
+        if (repo !== null) {
             let newRepo = { ...repo };
             newRepo.likes += change;
-            replaceRepo(repo,newRepo);
+            replaceRepo(repo, newRepo);
         }
     }
 
-    function findRepo(repoId){
+    function findRepo(repoId) {
         let repo = repoList.find((repo) => repo.id === repoId);
-        if(!repo) return null;
+        if (!repo) return null;
 
         return repo;
     }
 
-    function replaceRepo(oldValue, newValue){
-         let indx = repoList.findIndex((v) => v.id === oldValue.id);
-         if(indx !== -1){
-             repoList.splice(indx,1);
-             repoList.splice(indx,0,newValue);
-             let newList = [...repoList];
-             setRepoList(newList);
-         }
+    function replaceRepo(oldValue, newValue) {
+        let indx = repoList.findIndex((v) => v.id === oldValue.id);
+        if (indx !== -1) {
+            repoList.splice(indx, 1);
+            repoList.splice(indx, 0, newValue);
+            let newList = [...repoList];
+            setRepoList(newList);
+        }
     }
 
     return (
-        <div style={{marginTop:40}} className="container">
-               <Search onSearchClick={searchRepository}/>
-               {
-                   repoList.length === 0? (
-                       <h6 className="is-size-6 has-text-grey">No Repositories Found.</h6>
-                   ):(
-                       repoList.map((item) => (
-                           <RepoInfoCard changeLikes={updateLikes} item={ item } key={item.id}/>
-                       ))
-                   )
-               }
+        <>
+        <div className="columns" style={{marginTop:20}}>
+            <div className="column is-10 is-offset-1">
+                <Search onSearchClick={searchRepository} />
+            </div>
         </div>
+        <div className="columns">
+             <div className="column is-10 is-offset-1">
+            {
+                repoList.length === 0 ? (
+                    <h6 className="is-size-6 has-text-grey">No Repositories Found.</h6>
+                ) : (
+                        repoList.map((item) => (
+                            <RepoInfoCard changeLikes={updateLikes} item={item} key={item.id} />
+                        ))
+                    )
+            }
+            </div>
+        </div>
+        </>
     );
 }
 
